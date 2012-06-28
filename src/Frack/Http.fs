b73890b00ac62e692316770f31d9b6810e91e332
Ryan Riley <ryan.riley@panesofglass.org>
2012-06-28 00:32:13 -0500

    Clean up Http.fs.

--- src/Frack/Http.fs
+++ src/Frack/Http.fs
@@ -16,74 +16,9 @@
 //----------------------------------------------------------------------------
 module Frack.Http
 
-open System
-open System.Collections.Generic
-open System.Diagnostics.Contracts
-open System.IO
 open System.Net
-open System.Text
-open FSharp.Control
-open FSharp.IO
-open FSharpx
-open FSharpx.Iteratee
-open FSharpx.Iteratee.Binary
 open Frack
 open Frack.Sockets
-open Owin
-
-module Parser =
-
-    let readHeaders =
-        let rec lines cont = readLine >>= fun bs -> skipNewline >>= check cont bs
-        and check cont bs count =
-            match bs, count with
-            | bs, _ when ByteString.isEmpty bs -> Done(cont [], EOF)
-            | bs, 0 -> Done(cont [bs], EOF)
-            | _ -> lines <| fun tail -> cont <| bs::tail
-        lines id
-
-    let private parseStartLine (line: string, request: Request) =
-        let arr = line.Split([|' '|], 3)
-        request.Environment.Add(Request.Version, "1.0")
-        request.Environment.Add(Request.Method, arr.[0])
-
-        let uri = Uri(arr.[1], UriKind.RelativeOrAbsolute)
-        request.Environment.Add("frack.RequestUri", uri)
-
-        // TODO: Fix this so that the path can be determined correctly.
-        request.Environment.Add(Request.PathBase, "")
-
-        if uri.IsAbsoluteUri then
-            request.Environment.Add(Request.Path, uri.AbsolutePath)
-            request.Environment.Add(Request.QueryString, uri.Query)
-            request.Environment.Add(Request.Scheme, uri.Scheme)
-            request.Headers.Add("Host", [|uri.Host|])
-        else
-            request.Environment.Add(Request.Path, uri.OriginalString)
-
-        let version = Version.Parse(arr.[2].TrimStart("HTP/".ToCharArray()))
-        request.Environment.Add("frack.HttpVersion", version)
-
-    let private parseHeader (header: string, request: Owin.Request) =
-        // TODO: Proper header parsing.
-        let pair = header.Split([|':'|], 2)
-        if pair.Length > 1 then
-            request.Headers.[pair.[0]] <- [| pair.[1].TrimStart(' ') |]
-
-    let parse source = async {
-        let! result, source' = source |> connect readHeaders
-        match result with
-        | [] -> return Request.None
-        | startLine::headers ->
-            let request = {
-                Environment = new Dictionary<_,_>(HashIdentity.Structural)
-                Headers = new Dictionary<_,_>(HashIdentity.Structural)
-                Body = source'
-            }
-            parseStartLine (startLine |> ByteString.toString, request)
-            headers |> List.iter (fun h -> parseHeader(h |> ByteString.toString, request))
-            return request
-    }
 
 type Server (app, ?backlog, ?bufferSize) =
     let backlog = defaultArg backlog 1000
@@ -97,7 +32,7 @@ type Server (app, ?backlog, ?bufferSize) =
         let pool = BufferPool(backlog, bufferSize)
 
         let tcp = Tcp.Server(fun socket -> async {
-            let! request = Parser.parse <| socket.ReceiveAsyncSeq(pool)
+            let! request = Request.parse <| socket.ReceiveAsyncSeq(pool)
             let! response = app request
             do! socket.SendAsyncSeq(Response.toBytes response, pool)
         }, backlog)
