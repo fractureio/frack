0cad04dbbafd86a1b171746247e9bc0c2830f71e
Ryan Riley <ryan.riley@panesofglass.org>
2013-03-14 22:12:47 -0500

    Update OWIN to 1.0 spec.

--- src/Frack/Owin.fs
+++ src/Frack/Owin.fs
@@ -17,47 +17,144 @@
 namespace Owin
 
 open System
+open System.Collections.Concurrent
 open System.Collections.Generic
 open System.Threading
 open System.Threading.Tasks
+open Microsoft.FSharp.Core
 open FSharp.Control
-open FSharpx
 
-type BS = ByteString
-
-type MessageBody = AsyncSeq<BS>
-
-type Request = {
-    Environment : IDictionary<string, obj>
-    Headers : IDictionary<string, string[]>
-    Body : MessageBody
-}
-with static member None = { Environment = null; Headers = null; Body = AsyncSeq.empty }
-
-type Response = {
-    StatusCode : int
-    Headers : IDictionary<string, string[]>
-    Body : MessageBody
-    Properties : IDictionary<string, obj>
-}
-
-type Application = Request -> Async<Response>
-
-[<Interface>]
-type IAppBuilder =
-    abstract member Use : 'a -> 'a -> IAppBuilder
-    abstract member Build : IAppBuilder -> unit
-    abstract member AddAdapters : ('a -> 'b) * ('b -> 'a) -> IAppBuilder
-    abstract member Properties : IDictionary<string, obj> with get
+(**
+ * OWIN 1.0.0
+ * http://owin.org/spec/owin-1.0.0.html
+ *)
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Request =
-    let Version = "owin.Version"
-    let Method = "owin.RequestMethod"
-    let Scheme = "owin.RequestScheme"
-    let PathBase = "owin.RequestPathBase"
-    let Path = "owin.RequestPath"
-    let QueryString = "owin.RequestQueryString"
-    let Protocol = "owin.RequestProtocol"
-    let Headers = "owin.RequestHeaders"
-    let Body = "owin.RequestBody"
+module Constants =
+    (* 3.2.1 Request Data *)
+    [<CompiledName("RequestScheme")>]
+    let [<Literal>] requestScheme = "owin.RequestScheme"
+    [<CompiledName("RequestMethod")>]
+    let [<Literal>] requestMethod = "owin.RequestMethod"
+    [<CompiledName("RequestPathBase")>]
+    let [<Literal>] requestPathBase = "owin.RequestPathBase"
+    [<CompiledName("RequestPath")>]
+    let [<Literal>] requestPath = "owin.RequestPath"
+    [<CompiledName("RequestQueryString")>]
+    let [<Literal>] requestQueryString = "owin.RequestQueryString"
+    [<CompiledName("RequestProtocol")>]
+    let [<Literal>] requestProtocol = "owin.RequestProtocol"
+    [<CompiledName("RequestHeaders")>]
+    let [<Literal>] requestHeaders = "owin.RequestHeaders"
+    [<CompiledName("RequestBody")>]
+    let [<Literal>] requestBody = "owin.RequestBody"
+
+    (* 3.2.2 Response Data *)
+    [<CompiledName("ResponseStatusCode")>]
+    let [<Literal>] responseStatusCode = "owin.ResponseStatusCode"
+    [<CompiledName("ResponseReasonPhrase")>]
+    let [<Literal>] responseReasonPhrase = "owin.ResponseReasonPhrase"
+    [<CompiledName("ResponseProtocol")>]
+    let [<Literal>] responseProtocol = "owin.ResponseProtocol"
+    [<CompiledName("ResponseHeaders")>]
+    let [<Literal>] responseHeaders = "owin.ResponseHeaders"
+    [<CompiledName("ResponseBody")>]
+    let [<Literal>] responseBody = "owin.ResponseBody"
+
+    (* 3.2.3 Other Data *)
+    [<CompiledName("CallCancelled")>]
+    let [<Literal>] callCancelled = "owin.CallCancelled"
+    [<CompiledName("OwinVersion")>]
+    let [<Literal>] owinVersion = "owin.Version"
+
+    (* http://owin.org/spec/CommonKeys.html *)
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module CommonKeys =
+        [<CompiledName("ClientCertificate")>]
+        let [<Literal>] clientCertificate = "ssl.ClientCertificate"
+        [<CompiledName("RemoteIpAddress")>]
+        let [<Literal>] remoteIpAddress = "server.RemoteIpAddress"
+        [<CompiledName("RemotePort")>]
+        let [<Literal>] remotePort = "server.RemotePort"
+        [<CompiledName("LocalIpAddress")>]
+        let [<Literal>] localIpAddress = "server.LocalIpAddress"
+        [<CompiledName("LocalPort")>]
+        let [<Literal>] localPort = "server.LocalPort"
+        [<CompiledName("IsLocal")>]
+        let [<Literal>] isLocal = "server.IsLocal"
+        [<CompiledName("TraceOutput")>]
+        let [<Literal>] traceOutput = "host.TraceOutput"
+        [<CompiledName("Addresses")>]
+        let [<Literal>] addresses = "host.Addresses"
+        [<CompiledName("Capabilities")>]
+        let [<Literal>] capabilities = "server.Capabilities"
+        [<CompiledName("OnSendingHeaders")>]
+        let [<Literal>] onSendingHeaders = "server.OnSendingHeaders"
+
+    (* http://owin.org/extensions/owin-SendFile-Extension-v0.3.0.htm *)
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module SendFiles =
+        // 3.1. Startup
+        [<CompiledName("Version")>]
+        let [<Literal>] version = "sendfile.Version"
+        [<CompiledName("Support")>]
+        let [<Literal>] support = "sendfile.Support"
+        [<CompiledName("Concurrency")>]
+        let [<Literal>] concurrency = "sendfile.Concurrency"
+
+        // 3.2. Per Request
+        [<CompiledName("SendAsync")>]
+        let [<Literal>] sendAsync = "sendfile.SendAsync"
+
+    (* http://owin.org/extensions/owin-OpaqueStream-Extension-v0.3.0.htm *)
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module Opaque =
+        // 3.1. Startup
+        [<CompiledName("Version")>]
+        let [<Literal>] version = "opaque.Version"
+
+        // 3.2. Per Request
+        [<CompiledName("Upgrade")>]
+        let [<Literal>] upgrade = "opaque.Upgrade"
+
+        // 5. Consumption
+        [<CompiledName("Stream")>]
+        let [<Literal>] stream = "opaque.Stream"
+        [<CompiledName("CallCanceled")>]
+        let [<Literal>] callCancelled = "opaque.CallCancelled"
+
+    // http://owin.org/extensions/owin-OpaqueStream-Extension-v0.3.0.htm
+    [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+    module WebSocket =
+        // 3.1. Startup
+        [<CompiledName("Version")>]
+        let [<Literal>] version = "websocket.Version"
+
+        // 3.2. Per Request
+        [<CompiledName("Accept")>]
+        let [<Literal>] accept = "websocket.Accept"
+
+        // 4. Accept
+        [<CompiledName("SubProtocol")>]
+        let [<Literal>] subProtocol = "websocket.SubProtocol"
+
+        // 5. Consumption
+        [<CompiledName("SendAsync")>]
+        let [<Literal>] sendAsync = "websocket.SendAsync"
+        [<CompiledName("ReceiveAsync")>]
+        let [<Literal>] receiveAsync = "websocket.ReceiveAsync"
+        [<CompiledName("CloseAsync")>]
+        let [<Literal>] closeAsync = "websocket.CloseAsync"
+        [<CompiledName("CallCancelled")>]
+        let [<Literal>] callCancelled = "websocket.CallCancelled"
+        [<CompiledName("ClientCloseStatus")>]
+        let [<Literal>] clientCloseStatus = "websocket.ClientCloseStatus"
+        [<CompiledName("ClientCloseDescription")>]
+        let [<Literal>] clientCloseDescription = "websocket.ClientCloseDescription"
+
+type OwinApp = IDictionary<string, obj> -> Task
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module OwinApp =
+    [<CompiledName("ToFunc")>]
+    let toFunc (app: OwinApp) = Func<_,_>(app)
